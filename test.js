5cf08c524c3b9673139133902c55d4d46e2d6205
ProjectMoon <knifed@gmail.com>
2011-08-20 19:53:02 -0400

    one-parameter async unwrapping

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 var compiler = require('./compiler');
 
-var program = compiler.compile('x = a!(); y = b!();');
+var program = compiler.compile('var x = a!(); var y = b!();');
 console.log(program);
